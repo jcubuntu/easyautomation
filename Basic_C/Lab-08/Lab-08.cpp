fe5896cfa0b5c2e87c4a194bce9f183d3482d9c3
Pakpoom <jcubuntu@yahoo.com>
2017-09-12 19:22:02 +0700

    lab8

---
+++ Basic_C/Lab-08/Lab-08.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void helloWorld(){
+   printf("Hello World\n");
+}
+
+void displayNum(int inputNum){
+   printf("Input number is : %d \n",inputNum);
+}
+
+int sum(int num1, int num2){
+   int output;
+   output = num1 + num2;
+   return output;
+}
+
+int main(){
+
+
+}
